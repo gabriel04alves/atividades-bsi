535a5114cc6906a892d3b828da6f948d37633f40
Gabriel <gabriel52.alves@gmail.com>
2023-06-18 10:44:24 -0300

    Aula 04 - Listas (Arrays)

--- alg-e-prog-I/Aula04.py
+++ alg-e-prog-I/Aula04.py
@@ -377,13 +377,63 @@ print(f"Soma dos elementos abaixo da diagonal principal: {soma}")
 
 # 24) Escreva um algoritmo que leia duas matrizes reais de dimensão 3 x 5, calcule e imprima a sua
 # soma.
-
+import random
+linhas1 = []
+print(f'Matriz 1: ')
+for i in range(0, 3):
+  linha1 = []
+  for x in range(0, 5):
+    valor1 = random.randint(10,99)
+    linha1.append(valor1)
+  linhas1.append(linha1)
+  for valor1 in linha1:
+    print(valor1, end=" | ")
+  print('')
+linhas2 = []
+print(f'\nMatriz 2: ')
+for i in range(0, 3):
+  linha2 = []
+  for x in range(0, 5):
+    valor2 = random.randint(10,99)
+    linha2.append(valor2)
+  linhas2.append(linha2)
+  for valor2 in linha2:
+    print(valor2, end=" | ")
+  print('')
+linhasSoma = []
+print(f'\nMatriz 1 + matriz 2: ')
+for i in range(len(linhas1)):
+  linhaSoma = []
+  for j in range(len(linhas1[i])):
+    valorSoma = linhas1[i][j] + linhas2[i][j]
+    linhaSoma.append(valorSoma)
+  linhasSoma.append(linhaSoma)
+  for valorSoma in linhaSoma:
+    print(valorSoma, end=" | ")
+  print('')
 
 
 # 25) Dada uma matriz MAT de 4 x 5 elementos, faça um algoritmo para somar os elementos de
 # cada linha gerando o vetor SOMALINHA. Em seguida, somar os elementos do vetor SOMALINHA
 # na variável TOTAL que deve ser impressa no final.
-
+import random
+somalinha = []
+linhas = []
+print(f'Matriz: ')
+for i in range(0, 4):
+  linha = []
+  for x in range(0, 5):
+    valor = random.randint(10,99)
+    linha.append(valor)
+  linhas.append(linha)
+  somalinha.append(sum(linha))
+  for valor in linha:
+    print(valor, end=" | ")
+  print('')
+print('')
+for j in range(0, 4):
+  print(f'Vetor somalinha {j+1}: {somalinha[j]}')
+print(f'\nSoma de todas as linhas: {sum(somalinha)}')
 
 
 # 26) Escreva um algoritmo para calcular e imprimir a soma de duas matrizes. Seja matriz A (m x n)
@@ -406,18 +456,131 @@ print(f"Soma dos elementos abaixo da diagonal principal: {soma}")
 #     d.1) cada produto em cada armazém;
 #     d.2) estoque em cada armazém;
 #     d.3) cada produto em todos os armazéns.
-
+import random
+import sys
+inserirDados = int(input('Escolha entre inserir os dados manualmente ou gerar aleatórios (1 || 2): '))
+entrada = []
+if inserirDados == 1:
+    for j in range(3):
+        for z in range(3):
+            valor = int(input(f'Insira a quantidade do produto {z+1} no armazém {j+1}: '))
+            entrada.append(valor)
+elif inserirDados == 2:
+    for _ in range(9):
+        valor = random.randint(10, 99)
+        entrada.append(valor)
+else:
+    print('Escolha inválida, tente novamente')
+    sys.exit()
+linhas = []
+print(f'\nMatriz:')
+print('Produto | Armazém 1 | Armazém 2 | Armazém 3')
+for i in range(0, 3):
+    linha = []
+    print(f'   {i+1}    |', end=' ')
+    for x in range(0, 3):
+        valor = entrada[i * 3 + x]
+        linha.append(valor)
+        print(f'{valor:<10}', end=' | ')
+    linhas.append(linha)
+    print('')
+custoProduto = []
+if inserirDados == 1:
+    for i in range(3):
+        custo = float(input(f'Insira o custo do produto {i+1} (R$): '))
+        custoProduto.append(custo)
+elif inserirDados == 2:
+    for _ in range(3):
+        custo = random.randint(10, 200)
+        custoProduto.append(custo)
+print("\nCustos dos produtos:")
+for i in range(3):
+    print(f"Produto {i+1}: R$ {custoProduto[i]}")
+print('')
+for i in range(3):
+    totalProdutoArmazem = sum(linhas[j][i] for j in range(3))
+    print(f"No armazém {i+1}, a quantidade total de produtos é: {totalProdutoArmazem} unidades.")
+maiorQtdProduto2 = 0
+armazemMaiorQtdProduto2 = 0
+for i in range(3):
+    qtdProduto = linhas[i][1]
+    if qtdProduto > maiorQtdProduto2:
+        maiorQtdProduto2 = qtdProduto
+        armazemMaiorQtdProduto2 = i + 1
+print(f"O armazém com a maior quantidade de produto 2 é o {armazemMaiorQtdProduto2}.")
+custoTotalArmazem = [0, 0, 0]
+custoTotalEstoque = [0, 0, 0]
+custoTotalProduto = 0
+for i in range(3):
+    for j in range(3):
+        qtdProduto = linhas[i][j]
+        custoProduto_armazem = qtdProduto * custoProduto[j]
+        custoTotalArmazem[i] += custoProduto_armazem
+        custoTotalEstoque[j] += custoProduto_armazem
+        custoTotalProduto += custoProduto_armazem
+print("\nCustos:")
+for i in range(3):
+    print(f"Armazém {i+1}:")
+    for j in range(3):
+        print(f"Custo do produto {j+1}: R$ {linhas[i][j]} x R$ {custoProduto[j]} = R$ {linhas[i][j] * custoProduto[j]}")
+    print(f"Custo total do armazém: R$ {custoTotalArmazem[i]}")
+    print(f"Custo total do estoque: R$ {custoTotalEstoque[i]}")
+    print("")
+print(f"Custo total de todos os produtos em todos os armazéns: R$ {custoTotalProduto}")
 
 
 # 28) Faça um algoritmo que monte uma estrutura de dados homogênea 10 x 30, onde o conteúdo
 # de cada elemento é igual à soma dos valores de seus índices.
-
+matriz = []
+for i in range(0, 10):
+    linha = []
+    for j in range(0, 30):
+        valor = i + j
+        linha.append(valor)
+    matriz.append(linha)
+print("Matriz:")
+for linha in matriz:
+    for valor in linha:
+        print(valor, end=" | ")
+    print('')
 
 
 # 29) Ler duas matrizes A e B, cada uma com 7 linhas e 1 coluna. Construir uma matriz C de 7 x 2,
 # onde a primeira coluna deverá ser formada pelos elementos da matriz A e a segunda coluna
 # deverá ser formada pelos elementos da matriz B.
-
+import random
+inserirDados = int(input('Escolha entre inserir os dados manualmente ou gerar aleatórios (1 || 2): '))
+matrizA = []
+matrizB = []
+if inserirDados == 1:
+    for j in range(7):
+        valorA = int(input(f'Insira um valor para a matriz A na posição {j+1}: '))
+        matrizA.append([valorA])
+    for j in range(7):
+        valorB = int(input(f'Insira um valor para a matriz B na posição {j+1}: '))
+        matrizB.append([valorB])
+elif inserirDados == 2:
+    for i in range(7):
+        valorA = random.randint(10, 99)
+        matrizA.append([valorA])
+    for i in range(7):
+        valorB = random.randint(10, 99)
+        matrizB.append([valorB])
+print("\nMatriz A:")
+for linha in matrizA:
+    print(linha[0])
+print("\nMatriz B:")
+for linha in matrizB:
+    print(linha[0])
+matrizC = []
+for k in range(7):
+    linha = [matrizA[k][0], matrizB[k][0]]
+    matrizC.append(linha)
+print("\nMatriz C:")
+for linha in matrizC:
+    for valor in linha:
+        print(valor, end=" ")
+    print()
 
 
 # 30) Suponha que nos é dada uma matriz de inteiros A com m=20 linhas e n=30 colunas, e um
@@ -436,13 +599,60 @@ print(f"Soma dos elementos abaixo da diagonal principal: {soma}")
 # • Se I < J, A[I][J] = I / J;
 # • Se I = J, A[I][J] = 0;
 # • Se I > J, A[I][J] = J / I;
-
+matriz = [[0] * 6 for _ in range(10)]
+for i in range(10):
+    for j in range(6):
+        if i < j:
+            matriz[i][j] = i / j
+        elif i > j:
+            matriz[i][j] = j / i
+for i in range(10):
+    for j in range(6):
+        print(matriz[i][j], end=" | ")
+    print()
 
 # 32) Uma matriz X[n][4] contém informações sobre alunos de uma universidade. Os índices das
 # linhas são os números de matrícula e os elementos da 1a, 2a, 3a e 4a colunas são,
 # respectivamente, idade, sexo (0 ou 1), curso (no. do curso) e nota. Fazer um algoritmo para
 # obter o aluno do sexo 0, curso 6, que obteve a melhor nota. Supor a inexistência de empate.
-
+import random
+inserirDados = input("Escolha entre inserir os dados manualmente ou gerar aleatórios (1 || 2): ")
+if inserirDados != "1" and inserirDados != "2":
+    inserirDados = "1"
+qtdAlunos = int(input("Quantidade de alunos: "))
+matrizX = []
+for i in range(qtdAlunos):
+    print(f"\nDados do Aluno {i+1}:")
+    if inserirDados == "1":
+        idade = int(input("Idade: "))
+        sexo = int(input("Sexo (0 ou 1): "))
+        curso = int(input("Curso (número do curso): "))
+        nota = float(input("Nota: "))
+    elif inserirDados == "2":
+        idade = random.randint(18, 30)
+        sexo = random.randint(0, 1)
+        curso = random.randint(1, 10)
+        nota = round(random.uniform(0, 10), 1)
+        print(f"Idade: {idade}")
+        print(f"Sexo: {sexo}")
+        print(f"Curso: {curso}")
+        print(f"Nota: {nota}")
+    else: break
+    matrizX.append([idade, sexo, curso, nota])
+melhorNota = -1
+aluno = 0
+for linha in matrizX:
+    if linha[1] == 0 and linha[2] == 6 and linha[3] > melhorNota:
+        melhorNota = linha[3]
+        aluno = linha
+if aluno:
+    print("\nAluno do sexo 0, curso 6, com a melhor nota:")
+    matricula = matrizX.index(aluno) + 1
+    print(f"Matrícula: {matricula}")
+    print(f"Idade: {aluno[0]}")
+    print(f"Nota: {aluno[3]}")
+else:
+    print("\nNenhum aluno do sexo 0, curso 6 encontrado.")
 
 
 # 33) Faça um algoritmo que leia uma matriz QUANT de 10 linhas por 10 colunas e imprima as
@@ -451,7 +661,50 @@ print(f"Soma dos elementos abaixo da diagonal principal: {soma}")
 # b) dê o somatório dos cubos da 2a linha;
 # c) dê o somatório dos elementos da diagonal principal;
 # d) dê o somatório total dos 100 elementos.
-
+import random
+inserirDados = input("Escolha entre inserir os dados manualmente ou gerar aleatórios (1 || 2): ")
+if inserirDados != "1" and inserirDados != "2":
+    inserirDados = "1"
+matriz = []
+if inserirDados == "1":
+    print("Preencha a matriz manualmente:")
+    for i in range(10):
+        linha = []
+        for j in range(10):
+            valor = int(input(f"Informe o valor para a posição ({i+1},{j+1}): "))
+            linha.append(valor)
+        matriz.append(linha)
+else:
+    print("Preenchendo a matriz aleatoriamente:")
+    for i in range(10):
+        linha = []
+        for j in range(10):
+            valor = random.randint(10, 99)
+            linha.append(valor)
+        matriz.append(linha)
+print("\nMatriz:")
+for linha in matriz:
+    for valor in linha:
+        print(valor, end=" | ")
+    print()
+somaQuadrados = 0
+for linha in matriz:
+    somaQuadrados += linha[0] ** 2
+somaCubos = 0
+for valor in matriz[1]:
+    somaCubos += valor ** 3
+somaDiagonal = 0
+for i in range(10):
+    somaDiagonal += matriz[i][i]
+somaTotal = 0
+for linha in matriz:
+    for valor in linha:
+        somaTotal += valor
+print("\nCaracterísticas da matriz:")
+print(f"a) Somatório dos quadrados da 1ª coluna: {somaQuadrados}")
+print(f"b) Somatório dos cubos da 2ª linha: {somaCubos}")
+print(f"c) Somatório dos elementos da diagonal principal: {somaDiagonal}")
+print(f"d) Somatório total dos 100 elementos: {somaTotal}")
 
 
 # 34) Sabe-se que a multiplicação de duas matrizes A e B só é possível se o número de colunas da
@@ -472,6 +725,28 @@ print(f"Soma dos elementos abaixo da diagonal principal: {soma}")
 # Portanto, fazer um algoritmo que, a partir da produção mensal de motores M1 e M2 e seus
 # respectivos custos e lucros, calcule o custo e o lucro em cada um dos meses e o custo e lucro
 # anuais.
-
-
-
+producaoMensal = [[30, 20], [5, 10], [7, 15], [12, 8], [9, 14], [16, 22], [20, 17], [13, 19], [11, 9], [21, 23], [24, 27]]
+custoLucro = [[10, 3], [15, 2]]
+if len(producaoMensal[0]) != len(custoLucro):
+    print("Erro: As dimensões das matrizes não são compatíveis para a multiplicação.")
+else:
+    custoMensal = []
+    lucroMensal = []
+    for i in range(len(producaoMensal)):
+        linhaCusto = []
+        linhaLucro = []
+        for j in range(len(custoLucro[0])):
+            valorCusto = producaoMensal[i][0] * custoLucro[0][j]
+            valorLucro = producaoMensal[i][1] * custoLucro[1][j]
+            linhaCusto.append(valorCusto)
+            linhaLucro.append(valorLucro)
+        custoMensal.append(linhaCusto)
+        lucroMensal.append(linhaLucro)
+    custoAnual = [sum(coluna) for coluna in zip(*custoMensal)]
+    lucroAnual = [sum(coluna) for coluna in zip(*lucroMensal)]
+    print("Custo e Lucro Mensal:")
+    for i in range(len(producaoMensal)):
+        print(f"Mês {i+1}: Custo = {custoMensal[i]}, Lucro = {lucroMensal[i]}")
+    print("\nCusto e Lucro Anual:")
+    print(f"Custo Anual: {custoAnual}")
+    print(f"Lucro Anual: {lucroAnual}")
