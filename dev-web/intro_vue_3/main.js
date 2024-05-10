7daa60cd822734e1ec80ec501890ec823d0c7856
Gabriel Alves <gabriel52.alves@gmail.com>
2024-05-10 20:12:04 -0300

    dev web: vue 3 - tutorial 9

--- dev-web/intro_vue_3/main.js
+++ dev-web/intro_vue_3/main.js
@@ -1,41 +1,36 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },            
-            ],
-            selectedVariant: 0,
-            onSale: 'is on sale'
+            cart: 0,
+            premium: true
+            // product: 'Socks',
+            // brand: 'Vue Mastery',
+            // selectedVariant: 0,
+            // details: ['50% cotton', '30% wool', '20% polyester'],
+            // variants: [
+            //   { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+            //   { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            // ]
         }
     },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
-        },
-        updateVariant(index) {
-            this.selectedVariant = index
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        },
-        subtitle() {
-            return this.brand + ' ' + this.product + ' ' + this.onSale
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        }
-    }
+    // methods: {
+    //     addToCart() {
+    //         this.cart += 1
+    //     },
+    //     updateVariant(index) {
+    //         this.selectedVariant = index
+    //     }
+    // },
+    // computed: {
+    //     title() {
+    //         return this.brand + ' ' + this.product
+    //     },
+    //     image() {
+    //         return this.variants[this.selectedVariant].image
+    //     },
+    //     inStock() {
+    //         return this.variants[this.selectedVariant].quantity
+    //     },
+    // }
 })
+
