cb30d86ce36d75d2effd33db15b6d87183cf1822
Gabriel Alves <gabriel52.alves@gmail.com>
2024-10-28 16:36:38 -0300

    feat: criação da api para Editora

--- dev-web-II/template_django_pdm/app/urls.py
+++ dev-web-II/template_django_pdm/app/urls.py
@@ -11,11 +11,13 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from core.views import UserViewSet
 from core.views import CategoriaViewSet
+from core.views import EditoraViewSet
 
 router = DefaultRouter()
 
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"usuarios", UserViewSet, basename="usuarios")
+router.register(r"editoras", EditoraViewSet)
 
 
 urlpatterns = [
