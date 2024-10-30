b3abbf9bf7d84e9a984162d98374cc0dd5faa3bc
Gabriel Alves <gabriel52.alves@gmail.com>
2024-10-30 12:26:36 -0300

    feat: criação da entidade para livro

--- dev-web-II/template_django_pdm/app/urls.py
+++ dev-web-II/template_django_pdm/app/urls.py
@@ -12,12 +12,16 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from core.views import UserViewSet
 from core.views import CategoriaViewSet
 from core.views import EditoraViewSet
+from core.views import AutorViewSet
+from core.views import LivroViewSet
 
 router = DefaultRouter()
 
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"usuarios", UserViewSet, basename="usuarios")
 router.register(r"editoras", EditoraViewSet)
+router.register(r"autores", AutorViewSet)
+router.register(r"livros", LivroViewSet)
 
 
 urlpatterns = [
