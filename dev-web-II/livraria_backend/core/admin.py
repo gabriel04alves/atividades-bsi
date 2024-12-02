97abbedaa610a5f9ab29d2ccddaa2204c398c8ee
Gabriel Alves <gabriel52.alves@gmail.com>
2024-12-02 08:14:11 -0300

    feat: uso de TabularInline no admin para itens da compra

--- dev-web-II/livraria_backend/core/admin.py
+++ dev-web-II/livraria_backend/core/admin.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core.models import Autor, Categoria, Editora, Livro, User, Compra
+from core.models import Autor, Categoria, Editora, Livro, User, Compra, ItensCompra
 
 
 @admin.register(User)
@@ -88,6 +88,16 @@ class LivroAdmin(admin.ModelAdmin):
     list_per_page = 25
 
 
+class ItensCompraInline(admin.TabularInline):
+    model = ItensCompra
+    extra = 1
+
+
 @admin.register(Compra)
 class CompraAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("usuario", "status")
+    search_fields = ("usuario", "status")
+    list_filter = ("usuario", "status")
+    ordering = ("usuario", "status")
+    list_per_page = 25
+    inlines = [ItensCompraInline]
