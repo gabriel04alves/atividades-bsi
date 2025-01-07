da6e3269cb4a62d2719c67efae60b9d4d57dff62
Gabriel Alves <gabriel52.alves@gmail.com>
2025-01-07 15:17:03 -0300

    feat: criação de uma compra a partir do usuário autenticado

--- dev-web-II/livraria_backend/core/serializers/compra.py
+++ dev-web-II/livraria_backend/core/serializers/compra.py
@@ -1,4 +1,10 @@
-from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField
+from rest_framework.serializers import (
+    CharField,
+    CurrentUserDefault,
+    HiddenField,
+    ModelSerializer,
+    SerializerMethodField,
+)
 from core.models import Compra, ItensCompra
 
 
@@ -31,6 +37,7 @@ class ItensCompraCreateUpdateSerializer(ModelSerializer):
 
 
 class CompraCreateUpdateSerializer(ModelSerializer):
+    usuario = HiddenField(default=CurrentUserDefault())
     itens = ItensCompraCreateUpdateSerializer(many=True)
 
     def create(self, validated_data):
