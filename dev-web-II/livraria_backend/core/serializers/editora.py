1c5adcce345e6d170074559b01b29b912069017b
Gabriel Alves <gabriel52.alves@gmail.com>
2025-01-07 15:49:23 -0300

    feat: formatando dados antes de salvar

--- dev-web-II/livraria_backend/core/serializers/editora.py
+++ dev-web-II/livraria_backend/core/serializers/editora.py
@@ -1,7 +1,11 @@
-from rest_framework.serializers import ModelSerializer 
+from rest_framework.serializers import ModelSerializer
 from core.models import Categoria, Editora
+
 
 class EditoraSerializer(ModelSerializer):
     class Meta:
         model = Editora
         fields = "__all__"
+
+    def validate_email(self, email):
+        return email.lower()
