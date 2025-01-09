d43e261bed09e49ec44ded36b9f384ff04b6c529
Gabriel Alves <gabriel52.alves@gmail.com>
2025-01-09 17:01:34 -0300

    feat: acrescentando a data da compra

--- dev-web-II/livraria_backend/core/serializers/compra.py
+++ dev-web-II/livraria_backend/core/serializers/compra.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import (
     CharField,
     CurrentUserDefault,
+    DateTimeField,
     HiddenField,
     ModelSerializer,
     SerializerMethodField,
@@ -24,11 +25,12 @@ class ItensCompraSerializer(ModelSerializer):
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.username", read_only=True)
     status = CharField(source="get_status_display", read_only=True)
+    data = DateTimeField(read_only=True)
     itens = ItensCompraSerializer(many=True, read_only=True)
 
     class Meta:
         model = Compra
-        fields = ("id", "usuario", "status", "total", "itens")
+        fields = ("id", "usuario", "status", "total", "data", "itens")
 
 
 class ItensCompraCreateUpdateSerializer(ModelSerializer):
@@ -92,4 +94,4 @@ class CompraListSerializer(ModelSerializer):
 
     class Meta:
         model = Compra
-        fields = ("id", "usuario", "itens", "total")
+        fields = ("id", "usuario", "data", "itens", "total")
